369aa6b06082eb4ca9cbcef6ceb624f4edcaa5dc
nanih98 <49982093+nanih98@users.noreply.github.com>
2020-03-17 13:05:26 +0100

    Update bootstrap.sh

--- Docker/bootstrap.sh
+++ Docker/bootstrap.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+# Script para clonar los ficheros desde la repo oficial de github. El docker es capaz de clonarlo cuando se levanta por primera vez, pero da errores, por lo que, lo hacemos a mano.
+
+apt-get update && apt-get install git -y 
+
+mkdir -p volumes/teampass
 git clone https://github.com/nilsteampassnet/TeamPass.git volumes/teampass
-
-
-
